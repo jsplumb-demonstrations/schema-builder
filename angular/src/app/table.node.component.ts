2aef41135d72370ea0f9e1460be28cee6f6d3644
Simon Porritt <simon@jsplumbtoolkit.com>
2025-04-21 11:58:28 +1000

    - use `@for` in template instead of `*ngFor`

--- angular/src/app/table.node.component.ts
+++ angular/src/app/table.node.component.ts
@@ -4,7 +4,6 @@ import { uuid } from "@jsplumbtoolkit/browser-ui"
 import {BaseEditableNodeComponent} from "./base.editable.node"
 import {DATATYPE_VARCHAR} from "./constants"
 
-
 @Component({
   template:`<div class="jtk-schema-table jtk-schema-element">
     <div class="jtk-schema-element-name">
@@ -16,7 +15,9 @@ import {DATATYPE_VARCHAR} from "./constants"
       </div>
     </div>
     <div class="jtk-schema-columns">
-      <db-column *ngFor="let column of obj['columns']" [obj]="column" [parent]="this"></db-column>
+      @for(column of obj['columns'];track column) {
+        <db-column [obj]="column" [parent]="this"></db-column>
+      }
     </div>
   </div>
   `
@@ -25,14 +26,14 @@ export class TableNodeComponent extends BaseEditableNodeComponent {
 
   addColumn():void {
     //
-    // this is a helper method provided by the Toolkit's `BaseNodeComponent`. It will add a new port to the data
-    // for the node this component represents.  Under the hood the Toolkit does two things in response to this:
+    // this is a helper method provided by JsPlumb's `BaseNodeComponent`. It will add a new port to the data
+    // for the node this component represents.  Under the hood JsPlumb does two things in response to this:
     //
     // 1. It uses the `portDataProperty` that we set on the `toolkitParams` in AppComponent to find the array in
     // each node's backing data into which to write the a JS object for this port. Angular then discovers that
     // via change detection and renders a column component for it.
     //
-    // 2. it registers the new port data on the node in the Toolkit's data model.
+    // 2. it registers the new port data on the node in JsPlumb's data model.
     //
     this.addNewPort("column",{
       id:uuid(),
